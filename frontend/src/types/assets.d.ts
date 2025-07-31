b39fc5c973e4dbbeb6bd33b3ccb2c2081447aa31
Daria Naumova <darianaumova5@gmail.com>
2025-07-31 22:14:34 +0100

    update tsconfig

---
+++ frontend/src/types/assets.d.ts
@@ -0,0 +1,14 @@
+declare module "*.lottie" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
